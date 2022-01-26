c7870dac2f57f060445d48132e2575ed0dc423ae
Miguel Angel Trujillo <m.trujillo@globant.com>
2022-01-25 21:16:54 -0500

    jan/25 learning

--- DataStructures/src/Main.java
+++ DataStructures/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        /*
         Array numbers = new Array(3);
         numbers.insert(10);
         numbers.insert(20);
@@ -7,5 +8,13 @@ public class Main {
         numbers.insert(40);
         numbers.removeAt(3);
         numbers.print();
+        */
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.removeLast();
+        System.out.println(list.contains(10));
+
     }
 }
