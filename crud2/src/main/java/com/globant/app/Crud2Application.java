a8de716ece9e523e7c291611d73cc2cdb13cb0cb
Miguel Angel Trujillo <m.trujillo@globant.com>
2022-02-27 20:34:26 -0500

    another crud example

---
+++ crud2/src/main/java/com/globant/app/Crud2Application.java
@@ -0,0 +1,16 @@
+package com.globant.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class Crud2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crud2Application.class, args);
+	}
+
+
+}
---
+++ crud2/src/main/java/com/globant/app/student/StudentController.java
@@ -0,0 +1,42 @@
+package com.globant.app.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return studentService.getStudent();
+    }
+
+    @PostMapping
+    public void registrerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable("id") Long id){
+        studentService.deleteStudent(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateStudent(@PathVariable("id") Long id, @RequestParam(required = false) String name, @RequestParam(required = false) String email){
+        studentService.updateStudent(id, name, email);
+    }
+
+}
